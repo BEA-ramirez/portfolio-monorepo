4659d58fd47c57142a6f22d7930666ed851782a6
Bea Erin Angel L. Ramirez <21-1-01483@vsu.edu.ph>
2026-08-17 20:12:16 +0800

    Axios url points to proxy

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
   // production uses proxy, locally uses localhost 4000
   baseURL:
     process.env.NODE_ENV === "production"
-      ? undefined
+      ? "/"
       : process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
